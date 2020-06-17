cc9e7411c70655395399cbd3ae7755fa966c5e5f
Ram Tadepalli <rampraveen1995@gmail.com>
2020-06-17 20:42:21 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+ public static void main(String args[]){
+	while(true){
+           System.out.println("welcome to the programming");
+		try{
+		     Thread.sleep(3000);
+		   } catch(Exception  ex){
+        		System.out.println(ex);
+			}
+		    }
+	}
+}
